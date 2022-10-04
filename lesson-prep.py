3492ec63c9f3f715ed43ba7cd62dfea6924e122b
Atholton Computer Science Club <113360329+atholtoncompsci@users.noreply.github.com>
2022-10-04 07:51:59 -0400

    Create lesson-prep.py

---
+++ lesson-prep.py
@@ -0,0 +1,20 @@
+#IGNORE THIS FILE
+
+'''
+TOPICS:
+Print
+Comments
+Variables (Int, string, booleans, naming conventions)
+Operators (arithmetic, assignment
+Inputs
+If, elif, else
+While
+
+TUTORIALS:
+Hello World
+Calculate age given birth year
+Input name and birth year
+'''
+
+
+#Hello World
